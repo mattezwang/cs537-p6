eac4d1fd59d6d112bfdead4c058488a623a0c2c8
Matthew Wang <mattezwang@gmail.com>
2024-11-25 23:32:40 -0600

    trying not to align the stuff

--- solution/mkfs.c
+++ solution/mkfs.c
@@ -20,18 +20,15 @@ void init_sb(struct wfs_sb *sb) {
 
     // assigns the inode bitmap
     sb->i_bitmap_ptr = sizeof(struct wfs_sb);
-    // aligns it
-    sb->i_bitmap_ptr = (sb->i_bitmap_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
-
 
     sb->d_bitmap_ptr = sb->i_bitmap_ptr + ((num_inodes + 7) / 8);
-    sb->d_bitmap_ptr = (sb->d_bitmap_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
+    // sb->d_bitmap_ptr = (sb->d_bitmap_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
 
     sb->i_blocks_ptr = sb->d_bitmap_ptr + ((num_data_blocks + 7) / 8);
-    sb->i_blocks_ptr = (sb->i_blocks_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
+    // sb->i_blocks_ptr = (sb->i_blocks_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
 
     sb->d_blocks_ptr = sb->i_blocks_ptr + (num_inodes * BLOCK_SIZE);
-    sb->d_blocks_ptr = (sb->d_blocks_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
+    // sb->d_blocks_ptr = (sb->d_blocks_ptr + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
 }
 
 void init_disks() {
