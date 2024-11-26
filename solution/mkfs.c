0a771ecfd0a60a71ab65f93606011a8a41a537be
Matthew Wang <mattezwang@gmail.com>
2024-11-25 20:27:14 -0600

    chat's changes

--- solution/mkfs.c
+++ solution/mkfs.c
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdint.h>
-#include <sys/stat.h>
 
 int raid_mode;
 int num_inodes;
@@ -23,13 +22,6 @@ typedef struct {
     int data_start;
 } superblock_t;
 
-typedef struct {
-    int mode;    // File mode (directory, regular file, etc.)
-    int size;    // File size in bytes
-    int direct[10]; // Direct block pointers
-    int indirect;   // Indirect block pointer
-} inode_t;
-
 void parse_arguments(int argc, char *argv[]) {
     int tag;
     while ((tag = getopt(argc, argv, "r:d:i:b:")) != -1) {
@@ -37,51 +29,64 @@ void parse_arguments(int argc, char *argv[]) {
             case 'r':
                 raid_mode = atoi(optarg);
                 if (raid_mode > 1 || raid_mode < 0) {
-                    fprintf(stderr, "RAID mode out of bounds (must be 0 or 1)\n");
+                    fprintf(stderr, "raid mode out of bounds (has to be 0 or 1)\n");
                     exit(255);
                 }
                 break;
+
             case 'd':
                 if (num_disks >= capacity) {
                     capacity *= 2;
-                    disk_images = realloc(disk_images, capacity * sizeof(char *));
+                    disk_images = realloc(disk_images, (capacity) * sizeof(char *));
                 }
                 if (disk_images == NULL) {
-                    fprintf(stderr, "Disk images array allocation failed\n");
+                    fprintf(stderr, "disk_images array allocation failed\n");
                     exit(255);
                 }
                 disk_images[num_disks++] = strdup(optarg);
                 break;
+
             case 'i':
                 num_inodes = atoi(optarg);
                 if (num_inodes <= 0) {
-                    fprintf(stderr, "Number of inodes must be > 0\n");
+                    fprintf(stderr, "num of inodes has to be >= 0\n");
                     exit(255);
                 }
                 break;
+
             case 'b':
                 num_data_blocks = atoi(optarg);
                 if (num_data_blocks <= 0) {
-                    fprintf(stderr, "Number of data blocks must be > 0\n");
+                    fprintf(stderr, "num of data blocks has to be >= 0\n");
                     exit(255);
                 }
-                num_data_blocks = (num_data_blocks + 31) & ~31; // Round up to multiple of 32
+
+                // this makes it a multiple of 32
+                num_data_blocks = (num_data_blocks + 31) & ~31;
                 break;
+
             default:
-                fprintf(stderr, "Usage: %s -r <raid_mode> -d <disk> -i <inodes> -b <blocks>\n", argv[0]);
+                fprintf(stderr, "usage: %s -r <raid_mode> -d <disk> -i <inodes> -b <blocks>\n", argv[0]);
                 exit(255);
         }
     }
 }
 
+// New function: Initialize the filesystem on the specified disk
 void initialize_filesystem() {
-    int superblock_size = sizeof(superblock_t);
-    int inode_bitmap_size = (num_inodes + 7) / 8; // 1 bit per inode
-    int data_bitmap_size = (num_data_blocks + 7) / 8; // 1 bit per data block
-    int inode_region_size = num_inodes * 512; // Each inode is aligned to 512 bytes
+    int inode_bitmap_size = (num_inodes + 7) / 8; // Bitmap size in bytes (1 bit per inode)
+    int data_bitmap_size = (num_data_blocks + 7) / 8; // Bitmap size in bytes (1 bit per data block)
+    int inode_region_size = num_inodes * 512; // Each inode takes 512 bytes
     int data_region_size = num_data_blocks * 512;
 
-    int required_size = superblock_size + inode_bitmap_size + data_bitmap_size + inode_region_size + data_region_size;
+    // Calculate required size for the disk
+    int required_size = sizeof(superblock_t)       // Superblock
+                        + inode_bitmap_size       // Inode bitmap
+                        + data_bitmap_size        // Data bitmap
+                        + inode_region_size       // Inode region
+                        + data_region_size;       // Data region
+
+    char *zero_block = calloc(1, 512); // A zeroed-out 512-byte block
 
     for (int i = 0; i < num_disks; i++) {
         int fd = open(disk_images[i], O_RDWR | O_CREAT, 0666);
@@ -90,11 +95,11 @@ void initialize_filesystem() {
             exit(255);
         }
 
-        struct stat st;
-        fstat(fd, &st);
-        if (st.st_size < required_size) {
-            fprintf(stderr, "Disk image %s is too small for the filesystem\n", disk_images[i]);
+        // Ensure the disk image has enough size
+        if (ftruncate(fd, required_size) < 0) {
+            perror("Failed to set disk image size");
             close(fd);
+            free(zero_block);
             exit(255);
         }
 
@@ -103,41 +108,78 @@ void initialize_filesystem() {
             .raid_mode = raid_mode,
             .num_inodes = num_inodes,
             .num_data_blocks = num_data_blocks,
-            .inode_bitmap_start = superblock_size,
-            .data_bitmap_start = superblock_size + inode_bitmap_size,
-            .inode_start = superblock_size + inode_bitmap_size + data_bitmap_size,
-            .data_start = superblock_size + inode_bitmap_size + data_bitmap_size + inode_region_size
+            .inode_bitmap_start = sizeof(superblock_t),
+            .data_bitmap_start = sizeof(superblock_t) + inode_bitmap_size,
+            .inode_start = sizeof(superblock_t) + inode_bitmap_size + data_bitmap_size,
+            .data_start = sizeof(superblock_t) + inode_bitmap_size + data_bitmap_size + inode_region_size
         };
         lseek(fd, 0, SEEK_SET);
-        write(fd, &sb, sizeof(superblock_t));
+        if (write(fd, &sb, sizeof(superblock_t)) != sizeof(superblock_t)) {
+            perror("Failed to write superblock");
+            close(fd);
+            free(zero_block);
+            exit(255);
+        }
 
-        // Write zeroed-out bitmaps
-        char *zero_bitmap = calloc(1, inode_bitmap_size > data_bitmap_size ? inode_bitmap_size : data_bitmap_size);
+        // Write zeroed-out inode bitmap
         lseek(fd, sb.inode_bitmap_start, SEEK_SET);
-        write(fd, zero_bitmap, inode_bitmap_size);
-        lseek(fd, sb.data_bitmap_start, SEEK_SET);
-        write(fd, zero_bitmap, data_bitmap_size);
-        free(zero_bitmap);
+        for (int j = 0; j < inode_bitmap_size; j++) {
+            if (write(fd, zero_block, 1) != 1) {
+                perror("Failed to write inode bitmap");
+                close(fd);
+                free(zero_block);
+                exit(255);
+            }
+        }
 
-        // Write root inode
-        inode_t root_inode = {
-            .mode = S_IFDIR | 0755, // Directory with permissions
-            .size = 0,
-            .direct = {0}, // All direct pointers zeroed
-            .indirect = 0
-        };
+        // Write zeroed-out data bitmap
+        lseek(fd, sb.data_bitmap_start, SEEK_SET);
+        for (int j = 0; j < data_bitmap_size; j++) {
+            if (write(fd, zero_block, 1) != 1) {
+                perror("Failed to write data bitmap");
+                close(fd);
+                free(zero_block);
+                exit(255);
+            }
+        }
+
+        // Write zeroed-out inodes
         lseek(fd, sb.inode_start, SEEK_SET);
-        write(fd, &root_inode, sizeof(inode_t));
+        for (int j = 0; j < num_inodes; j++) {
+            if (write(fd, zero_block, 512) != 512) {
+                perror("Failed to write inode");
+                close(fd);
+                free(zero_block);
+                exit(255);
+            }
+        }
+
+        // Write zeroed-out data blocks
+        lseek(fd, sb.data_start, SEEK_SET);
+        for (int j = 0; j < num_data_blocks; j++) {
+            if (write(fd, zero_block, 512) != 512) {
+                perror("Failed to write data block");
+                close(fd);
+                free(zero_block);
+                exit(255);
+            }
+        }
 
         close(fd);
     }
+    free(zero_block);
+
+    printf("Filesystem initialized successfully.\n");
 }
 
 int main(int argc, char *argv[]) {
     disk_images = malloc(capacity * sizeof(char *));
     parse_arguments(argc, argv);
+
+    // Call the function to initialize the filesystem
     initialize_filesystem();
 
+    // Free allocated memory
     for (int i = 0; i < num_disks; i++) {
         free(disk_images[i]);
     }
