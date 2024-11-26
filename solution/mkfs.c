c86e98f2a09d1600e50b9d087fe04d1885acaf6e
Matthew Wang <mattezwang@gmail.com>
2024-11-25 20:07:05 -0600

    old ver

--- solution/mkfs.c
+++ solution/mkfs.c
@@ -12,119 +12,6 @@ int capacity = 10;
 int num_disks;
 int num_data_blocks;
 
-typedef struct {
-    int raid_mode;
-    int num_inodes;
-    int num_data_blocks;
-    int inode_bitmap_start;
-    int data_bitmap_start;
-    int inode_start;
-    int data_start;
-} superblock;
-
-typedef struct {
-    uint32_t size;       // File size in bytes
-    uint16_t type;       // File type (e.g., directory = 1, file = 2)
-    uint16_t links;      // Number of links (e.g., . and .. for a directory)
-    uint32_t direct[10]; // Direct block pointers
-    uint32_t indirect;   // Indirect block pointer
-} inode_t;
-
-
-void calculate_layout(superblock *sb) {
-    sb->inode_bitmap_start = 1; // Superblock takes the first block
-    sb->data_bitmap_start = sb->inode_bitmap_start + (num_inodes + 7) / 8 / 512;
-    sb->inode_start = sb->data_bitmap_start + (num_data_blocks + 7) / 8 / 512;
-    sb->data_start = sb->inode_start + num_inodes;
-}
-
-void validate_and_initialize_disks(int required_size) {
-    for (int i = 0; i < num_disks; i++) {
-        int fd = open(disk_images[i], O_RDWR | O_CREAT, 0644);
-        if (fd < 0) {
-            perror("Failed to open or create disk image");
-            exit(-1);
-        }
-
-        // Check if the disk is large enough
-        off_t size = lseek(fd, 0, SEEK_END);
-        if (size < required_size) {
-            printf("Resizing disk image %s to %d bytes\n", disk_images[i], required_size);
-            if (ftruncate(fd, required_size) < 0) {
-                perror("Failed to resize disk image");
-                close(fd);
-                exit(-1);
-            }
-        }
-
-        // Zero out the disk
-        char zero[512] = {0};
-        lseek(fd, 0, SEEK_SET);
-        for (int j = 0; j < required_size / 512; j++) {
-            write(fd, zero, 512);
-        }
-
-        close(fd);
-    }
-}
-
-void write_metadata(superblock *sb) {
-    printf("Writing metadata to disks...\n");
-    for (int i = 0; i < num_disks; i++) {
-        int fd = open(disk_images[i], O_RDWR);
-        if (fd < 0) {
-            perror("Failed to open disk image");
-            exit(-1);
-        }
-
-        // Write superblock
-        lseek(fd, 0, SEEK_SET);
-        write(fd, sb, sizeof(superblock));
-
-        // Debug superblock writing
-        printf("Superblock written to disk %s\n", disk_images[i]);
-
-        // Write inode bitmap
-        char bitmap[512] = {0};
-        bitmap[0] = 1; // Mark the root inode as allocated
-        lseek(fd, sb->inode_bitmap_start * 512, SEEK_SET);
-        write(fd, bitmap, 512);
-
-        // Debug inode bitmap
-        printf("Inode bitmap written at block %d\n", sb->inode_bitmap_start);
-
-        // Write data block bitmap
-        lseek(fd, sb->data_bitmap_start * 512, SEEK_SET);
-        write(fd, bitmap, 512);
-
-        // Write empty inodes
-        char inode_block[512] = {0};
-        for (int j = 0; j < sb->num_inodes; j++) {
-            lseek(fd, (sb->inode_start + j) * 512, SEEK_SET);
-            write(fd, inode_block, 512);
-        }
-
-        // Debug inode writing
-        printf("Inodes written starting at block %d\n", sb->inode_start);
-
-        // Write root inode
-        inode_t root_inode = {0};
-        root_inode.type = 1;  // Directory type
-        root_inode.links = 2; // "." and ".."
-        char root_block[512] = {0};
-        memcpy(root_block, &root_inode, sizeof(inode_t));
-        lseek(fd, sb->inode_start * 512, SEEK_SET);
-        write(fd, root_block, 512);
-
-        printf("Root inode written at block %d\n", sb->inode_start);
-
-        close(fd);
-    }
-}
-
-
-
-
 void
 parse_arguments(int argc, char *argv[]) {
 
@@ -179,39 +66,109 @@ parse_arguments(int argc, char *argv[]) {
     }
 }
 
+typedef struct {
+    int raid_mode;          // RAID mode (0 or 1)
+    int num_inodes;         // Number of inodes
+    int num_data_blocks;    // Number of data blocks
+    int inode_bitmap_start; // Offset of inode bitmap
+    int data_bitmap_start;  // Offset of data block bitmap
+    int inode_start;        // Offset of inodes
+    int data_start;         // Offset of data blocks
+} superblock_t;
+
+
+void write_metadata(superblock_t *sb) {
+    for (int i = 0; i < num_disks; i++) {
+        int fd = open(disk_images[i], O_RDWR);
+        if (fd < 0) {
+            perror("Failed to open disk image");
+            exit(-1);
+        }
+
+        // Write superblock
+        lseek(fd, 0, SEEK_SET);
+        write(fd, sb, sizeof(superblock_t));
+
+        // Write empty inode bitmap
+        lseek(fd, sb->inode_bitmap_start * 512, SEEK_SET);
+        char bitmap[512] = {0};
+        for (int j = 0; j < (num_inodes + 7) / 8 / 512; j++) {
+            write(fd, bitmap, 512);
+        }
+
+        // Write empty data bitmap
+        lseek(fd, sb->data_bitmap_start * 512, SEEK_SET);
+        for (int j = 0; j < (num_data_blocks + 7) / 8 / 512; j++) {
+            write(fd, bitmap, 512);
+        }
+
+        // Write root inode
+        lseek(fd, sb->inode_start * 512, SEEK_SET);
+        char inode[512] = {0};
+        inode[0] = 1; // Mark as allocated
+        write(fd, inode, 512);
+
+        close(fd);
+    }
+}
+
+void validate_and_initialize_disks() {
+    for (int i = 0; i < num_disks; i++) {
+        int fd = open(disk_images[i], O_RDWR | O_CREAT, 0644);
+        if (fd < 0) {
+            perror("Failed to open disk image");
+            exit(-1);
+        }
+
+        // Calculate required size
+        int required_size = (1 +   // Superblock
+                             (num_inodes + 7) / 8 / 512 +  // Inode bitmap
+                             (num_data_blocks + 7) / 8 / 512 + // Data block bitmap
+                             num_inodes +  // Inodes
+                             num_data_blocks) * 512;
+
+        // Check if disk is large enough
+        off_t size = lseek(fd, 0, SEEK_END);
+        if (size < required_size) {
+            fprintf(stderr, "Disk image %s is too small\n", disk_images[i]);
+            exit(-1);
+        }
+
+        // Zero out the file
+        lseek(fd, 0, SEEK_SET);
+        char zero[512] = {0};
+        for (int j = 0; j < required_size / 512; j++) {
+            write(fd, zero, 512);
+        }
+
+        close(fd);
+    }
+}
+
+void calculate_layout(superblock_t *sb) {
+    sb->inode_bitmap_start = 1; // Superblock takes the first block
+    sb->data_bitmap_start = sb->inode_bitmap_start + (num_inodes + 7) / 8 / 512;
+    sb->inode_start = sb->data_bitmap_start + (num_data_blocks + 7) / 8 / 512;
+    sb->data_start = sb->inode_start + num_inodes;
+}
+
+
+
 
 int main(int argc, char *argv[]) {
-
     disk_images = malloc(capacity * sizeof(char*));
     parse_arguments(argc, argv);
-
-    // superblock sb;
-    // sb.raid_mode = raid_mode;
-    // sb.num_inodes = num_inodes;
-    // sb.num_data_blocks = num_data_blocks;
-
-    superblock sb;
+    
+    superblock_t sb;
     sb.raid_mode = raid_mode;
     sb.num_inodes = num_inodes;
     sb.num_data_blocks = num_data_blocks;
 
-    // Calculate layout
     calculate_layout(&sb);
-
-    // Calculate required size
-    int required_size = (1 +                        // Superblock
-                         (num_inodes + 7) / 8 / 512 +  // Inode bitmap
-                         (num_data_blocks + 7) / 8 / 512 + // Data block bitmap
-                         num_inodes +                  // Inodes
-                         num_data_blocks) * 512;
-
-    // Validate and initialize disk images
-    validate_and_initialize_disks(required_size);
-
-    // Write metadata to disk
+    validate_and_initialize_disks();
     write_metadata(&sb);
 
-
+    // Free resources
     for (int i = 0; i < num_disks; i++) {
         free(disk_images[i]);
     }
