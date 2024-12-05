bd36f4e651a18b08208d266dd132a1ff2a327ff6
Matthew Wang <mattezwang@gmail.com>
2024-12-04 20:38:53 -0600

    does fuse.h work on csl machine

--- solution/wfs.c
+++ solution/wfs.c
@@ -1,3 +1,13 @@
+// #define etc 30
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
 #define FUSE_USE_VERSION 30
 
-int main(void) {return 0;}
+int main(void) {
+
+    return 0;
+}
