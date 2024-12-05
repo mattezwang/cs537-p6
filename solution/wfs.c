c51c66f7569b96e0c326fdf68ab443328d48341c
Matthew Wang <mattezwang@gmail.com>
2024-12-04 20:40:36 -0600

    trying the fuse.h thing again

--- solution/wfs.c
+++ solution/wfs.c
@@ -1,11 +1,10 @@
 // #define etc 30
 
+#define FUSE_USE_VERSION 30
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-
-#define FUSE_USE_VERSION 30
 
 int main(void) {
 
