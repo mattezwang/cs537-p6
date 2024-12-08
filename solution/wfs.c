68f0e7bc775ecc0b7c58eaf287a055e338ba57c7
Matthew Wang <mattezwang@gmail.com>
2024-12-07 18:27:01 -0600

    changes

--- solution/wfs.c
+++ solution/wfs.c
@@ -362,38 +362,6 @@ static int wfs_getattr(const char *path, struct stat *stbuf) {
     return SUCCESS;
 }
 
-void list_directories() {
-
-    const char *path = "mnt";
-    struct dirent *entry;
-    DIR *dir = opendir(path);
-
-    if (!dir) {
-        perror("opendir");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Directories inside: %s\n", path);
-
-    while ((entry = readdir(dir)) != NULL) {
-        // Construct the full path to check if it's a directory
-        char full_path[1024];
-        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
-
-        struct stat entry_stat;
-        if (stat(full_path, &entry_stat) == 0 && S_ISDIR(entry_stat.st_mode)) {
-            // Skip "." and ".."
-            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-                printf("%s\n", entry->d_name);
-            }
-        }
-    }
-
-    closedir(dir);
-}
-
-
-
 static int wfs_mknod(const char *path, mode_t mode, dev_t dev) {
     printf("Entering wfs_mknod\n");
     printf("Inode mode: %o\n", mode);
